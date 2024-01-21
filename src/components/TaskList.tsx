70d6ff45c220a56937f6fdb82b14de819f53405c
Manh Nguyen <manh.nguyen@pfizer.com>
2024-01-21 10:33:26 +0700

    feat: adding Task and TaskList components

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,16 @@
+import Task from './Task';
+
+const TaskList = () => {
+
+  return (
+    <>
+      <main>
+        <Task id={1} title="First task" description="Reprehenderit magna Lorem dolor sint adipisicing!" />
+        <Task id={2} title="Second task" description="Enim sit eu id." />
+        <Task id={3} title="Third task" description="Ad amet ex ipsum tempor proident mollit culpa consectetur." />
+      </main>
+    </>
+  )
+}
+
+export default TaskList
