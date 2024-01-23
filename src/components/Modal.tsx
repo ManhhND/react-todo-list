fae60e7d63f1ec98535185eea17a936191a99a9a
Manh Nguyen <manh.nguyen@pfizer.com>
2024-01-23 16:57:51 +0700

    refactor: change modal form triggering from route to state

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,19 +1,16 @@
 import { ReactNode } from 'react'
-import { useNavigate } from 'react-router-dom'
 import classes from './Modal.module.css'
 
 const Modal = ({
-  children
+  children,
+  onClose
 }: {
   children: ReactNode
+  onClose: () => void
 }) => {
-  const navigate = useNavigate();
-  const handleClose = () => {
-    navigate('/')
-  }
   return (
     <>
-      <div className={classes.backdrop} onClick={handleClose}></div>
+      <div className={classes.backdrop} onClick={onClose}></div>
       <dialog open className={classes.modal}>{children}</dialog>
     </>
   )
