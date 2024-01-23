030486294ededbdcb078e02efd8a85aebeb0978e
Manh Nguyen <manh.nguyen@pfizer.com>
2024-01-23 16:56:29 +0700

    refactor: rework AddTask component, add NewTask button to TaskList

--- src/routes/AddTask.tsx
+++ src/routes/AddTask.tsx
@@ -1,55 +1,8 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '../app/hook';
-import Modal from '../components/Modal';
-import { addTask } from '../features/taskSlice';
-import classes from './AddTask.module.css';
+import TaskForm from "../components/TaskForm";
 
-const AddTask = () => {
-  const [enteredTitle, setEnteredTitle] = useState<string>('')
-  const [enteredDescription, setEnteredDescription] = useState<string>('')
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
+const AddTask = ({ onStopAdding }: { onStopAdding: () => void }) => {
 
-  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEnteredTitle(e.target.value)
-  }
-
-  const handleDescriptionChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setEnteredDescription(e.target.value)
-  }
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const submittedData = {
-      id: Math.round(Math.random() * 10000).toString(),
-      title: enteredTitle,
-      description: enteredDescription
-    }
-    dispatch(addTask(submittedData))
-    navigate('/')
-  }
-
-  return (
-    <>
-      <Modal>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name='title' required onChange={handleTitleChange} />
-          </p>
-          <p>
-            <label htmlFor="description">Description</label>
-            <textarea id="description" name='description' required rows={3} onChange={handleDescriptionChange} />
-          </p>
-          <p className={classes.actions}>
-            <Link type='button' to='/'>Cancel</Link>
-            <button>Add</button>
-          </p>
-        </form>
-      </Modal>
-    </>
-  );
+  return <TaskForm action="add" onModalClose={onStopAdding} />;
 }
 
 export default AddTask;
--- src/routes/TaskList.tsx
+++ src/routes/TaskList.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
+import { MdAddTask } from 'react-icons/md';
 import { Outlet } from 'react-router-dom';
 import { useAppSelector } from '../app/hook';
 import Task from '../components/Task';
 import { selectTask } from '../features/taskSlice';
+import AddTask from './AddTask';
 import classes from './TaskList.module.css';
 
 export type TaskItem = {
@@ -11,15 +14,28 @@ export type TaskItem = {
 }
 
 const TaskList = () => {
+  const [isAdding, setIsAdding] = useState<boolean>(false)
   const taskList = useAppSelector(selectTask)
+
+  const showModal = () => {
+    setIsAdding(true)
+  }
+
+  const hideModal = () => {
+    setIsAdding(false)
+  }
 
   return (
     <>
       <Outlet />
-      <main>
-        {/* <Task id={1} title="First task" description="Reprehenderit magna Lorem dolor sint adipisicing!" />
-        <Task id={2} title="Second task" description="Enim sit eu id." />
-        <Task id={3} title="Third task" description="Ad amet ex ipsum tempor proident mollit culpa consectetur." /> */}
+      <div>
+        <p>
+          <button className={classes.button} onClick={showModal}>
+            <MdAddTask size={18} />
+            New Task
+          </button>
+        </p>
+        {isAdding && <AddTask onStopAdding={hideModal} />}
         {taskList.length > 0 && (
           <ul className={classes.tasks}>
             {taskList.map((task: TaskItem) => <Task id={task.id} title={task?.title} description={task?.description} key={`task-${task.id}`} />)}
@@ -30,7 +46,7 @@ const TaskList = () => {
             <h2>There are no tasks. Let's add some!</h2>
           </div>
         )}
-      </main>
+      </div>
     </>
   )
 }
