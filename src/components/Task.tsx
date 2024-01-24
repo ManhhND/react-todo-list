303c950a9dce9623af54418c9f4d1286a78beb26
Manh Nguyen <manh.nguyen@pfizer.com>
2024-01-24 16:53:52 +0700

    feat: add action buttons (View, Edit, Delete) to task item and style

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,9 @@
-import { Link } from 'react-router-dom'
-import classes from './Task.module.css'
+import { useState } from 'react'
+import { FaEdit, FaEye, FaTrash } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../app/hook'
+import { deleteTask, selectTask } from '../features/taskSlice'
+import EditTask from '../routes/EditTask'
 
 const Task = ({
   id,
@@ -10,15 +14,47 @@ const Task = ({
   title: string,
   description: string
 }) => {
+  const [isEditting, setIsEditting] = useState<boolean>(false)
+  const taskList = useAppSelector(selectTask)
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const task = taskList.filter((task) => task.id === id)[0]
+
+  const showModal = () => {
+    setIsEditting(true)
+  }
+
+  const hideModal = () => {
+    setIsEditting(false)
+  }
+
+  const handleDeleteTask = () => {
+    dispatch(deleteTask(task))
+    navigate('/')
+  }
 
   return (
     <>
-      <div className={classes.task}>
-        <Link to={`/task/${id}`}>
-          <p className={classes.title}>{title}</p>
-          <p className={classes.description}>{description}</p>
-        </Link>
-      </div>
+      {isEditting && <EditTask onStopEditting={hideModal} taskData={task} />}
+      <li className="p-4 border-2 rounded-lg border-violet-500 mb-4 hover:bg-amber-400">
+        <div className="flex justify-between">
+          <div>
+            <h2 className="font-bold text-lg uppercase">{title}</h2>
+            <p className="italic">{description}</p>
+          </div>
+          <div className="flex gap-2 justify-center items-center">
+            <Link to={`/task/${id}`} className="text-violet-500" title="See detail">
+              <FaEye size={24} />
+            </Link>
+            <button className="text-violet-500" type="button" onClick={showModal} title="Edit">
+              <FaEdit size={24} />
+            </button>
+            <button className="text-red-500" onClick={handleDeleteTask} title="Delete">
+              <FaTrash size={24} />
+            </button>
+          </div>
+        </div>
+      </li>
     </>
   )
 }
