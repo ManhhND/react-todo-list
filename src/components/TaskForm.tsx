3ced1cb7d3170eac9b912b557c7bfe6aa3dec148
Manh Nguyen <manh.nguyen@pfizer.com>
2024-01-24 16:51:01 +0700

    style: change TaskForm style from css to tailwind

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -3,7 +3,6 @@ import { useAppDispatch } from "../app/hook";
 import { addTask, updateTask } from "../features/taskSlice";
 import { TaskItem } from "../routes/TaskList";
 import Modal from "./Modal";
-import classes from './TaskForm.module.css';
 
 const TaskForm = ({
   action,
@@ -49,19 +48,19 @@ const TaskForm = ({
   return (
     <>
       <Modal onClose={onModalClose}>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name='title' required onChange={handleTitleChange} defaultValue={taskData?.title} ref={titleRef} />
-          </p>
-          <p>
-            <label htmlFor="description">Description</label>
-            <textarea id="description" name='description' required rows={3} onChange={handleDescriptionChange} defaultValue={taskData?.description} ref={descriptionRef} />
-          </p>
-          <p className={classes.actions}>
-            <button type="button" onClick={onModalClose}>Cancel</button>
-            <button>{action === 'post' ? 'Add' : 'Update'}</button>
-          </p>
+        <form className="p-4" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="text-violet-500" htmlFor="title">Title</label>
+            <input className="w-full p-2 border border-violet-500 rounded-md" type="text" id="title" name='title' required onChange={handleTitleChange} defaultValue={taskData?.title} ref={titleRef} />
+          </div>
+          <div className="mb-4">
+            <label className="text-violet-500" htmlFor="description">Description</label>
+            <textarea className="block p-2 w-full border border-violet-500 rounded-md" id="description" name='description' required rows={5} onChange={handleDescriptionChange} defaultValue={taskData?.description} ref={descriptionRef} />
+          </div>
+          <div className="mt-4 flex gap-2 justify-center">
+            <button className="p-4 rounded-md hover:bg-black/[0.1] w-1/2 text-violet-500" type="button" onClick={onModalClose}>Cancel</button>
+            <button className="p-4 rounded-md bg-violet-500 hover:bg-violet-600 text-white w-1/2">{action === 'create' ? 'Create' : 'Update'}</button>
+          </div>
         </form>
       </Modal>
     </>
