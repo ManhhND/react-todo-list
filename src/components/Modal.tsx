cb1be9b84e038ccca9c3b514b5cfa4f0651e22bd
Manh Nguyen <manh.nguyen@pfizer.com>
2024-01-25 13:39:50 +0700

    style: Modal component update

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,8 +9,8 @@ const Modal = ({
 }) => {
   return (
     <>
-      <div className="bg-black/[0.6] fixed top-0 left-0 w-full h-full" onClick={onClose}></div>
-      <dialog open className="w-10/12 md:w-1/2 rounded-md">{children}</dialog>
+      <div className="bg-black/[0.6] fixed top-0 left-0 w-full h-full z-10" onClick={onClose}></div>
+      <dialog open className="w-10/12 md:w-1/2 rounded-md z-10">{children}</dialog>
     </>
   )
 }
